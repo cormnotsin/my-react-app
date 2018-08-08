c340ba1d9cf9d73da79afbaec63975de73f450f5
Ethan Selin <corm1337@gmail.com>
2018-08-07 20:41:35 -0700

    experimenting with dynamic nested routes

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,21 +2,22 @@ import React from 'react'
 import {
     Link,
     Route,
+    withRouter, //look this up
 } from 'react-router-dom';
 
-export default function ContactUs({match}) {
+export default function ContactUs({ match }) {
     return (
         <div>
             <h2>Contact Us</h2>
             <ul>
                 <li>
-                    <Link to={'${match.url}/facebook'}>Facebook</Link>
+                    <Link to={`${match.url}/facebook`}>Facebook</Link>
                 </li>
                 <li>
-                    <Link to={'${match.url}/email'}>Email</Link>
+                    <Link to={`${match.url}/email`}>Email</Link>
                 </li>
                 <li>
-                    <Link to={'${match.url}/instagram'}>Instagram</Link>
+                    <Link to={`${match.url}/instagram`}>Instagram</Link>
                 </li>
             </ul>
             <hr />

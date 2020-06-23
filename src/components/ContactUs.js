345559de2195c927df7529bb8120bc158bb5334d
Ethan Selin <corm1337@gmail.com>
2020-06-22 20:32:35 -0700

    working with dynamic routes for the contact us section

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,26 +1,41 @@
 import React from 'react'
+import Facebook from './Facebook';
+import Instagram from './Instagram';
+import Email from './Email';
 import {
     Link,
     Route,
     withRouter, //look this up
 } from 'react-router-dom';
 
+const contactUs = [
+    {
+        name: 'Facebook',
+        id: 'facebook'
+    },
+    {
+        name: 'Instagram',
+        id: 'instagram'
+    },
+    {
+        name: 'Email',
+        id: 'email'
+    },
+]
+
 export default function ContactUs({ match }) {
     return (
         <div>
             <h2>Contact Us</h2>
             <ul>
-                <li>
-                    <Link to={`${match.url}/facebook`}>Facebook</Link>
+                {contactUs.map(({ name, id }) => (
+                <li key={id}>
+                    <Link to={`${match.url}/${id}`}>{name}</Link>
                 </li>
-                <li>
-                    <Link to={`${match.url}/email`}>Email</Link>
-                </li>
-                <li>
-                    <Link to={`${match.url}/instagram`}>Instagram</Link>
-                </li>
+                ))}
             </ul>
             <hr />
+            <Route path={'${match.url}/${id}'} component={Facebook}/>
         </div>
     )
 }
